49fdc3eb470ed6deb8697384510ca321fcf258a5
James <a>
2025-05-23 17:35:20 +0700

    unable reload

--- local_ai/apis.py
+++ local_ai/apis.py
@@ -441,6 +441,5 @@ if __name__ == "__main__":
         "local_ai.apis:app",
         host="0.0.0.0",
         port=CONFIG["proxy_port"],
-        workers=CONFIG["workers"],
-        reload=True
+        workers=CONFIG["workers"]
     )
